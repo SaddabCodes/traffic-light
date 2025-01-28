cf7d8efed97a59b66080013fde7571abcaf72e6c
Saddab <saddabakhtar17@gmail.com>
2025-01-28 19:42:22 +0530

    update code

--- src/components/TrafficLight.jsx
+++ src/components/TrafficLight.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import "./TrafficLight.css";
+
 export default function TrafficLight() {
+  const [red, setRed] = useState(true);
+  const [yellow, setYellow] = useState(false);
+  const [green, setGreen] = useState(false);
+
+  useEffect(() => {
+    const lightColor= setInterval(() => {
+      if (red && !yellow && !green) {
+        setGreen(true);
+        setRed(false);
+      } else if (green && !red && !yellow) {
+        setYellow(true);
+        setGreen(false);
+      } else if (yellow && !red && !green) {
+        setYellow(false);
+        setRed(true);
+      }
+    }, 2000);
+    return ()=>{
+      clearInterval(lightColor)
+    }
+  }, [red,yellow,green]);
   return (
-    <div>
-      <h1>Traffic Light</h1>
+    <div className="container" >
+      <p className="circle" style={{ backgroundColor:`${green ?'green':'silver'}` }}></p>
+      <p className="circle" style={{ backgroundColor:`${yellow ?'yellow':'silver'}` }}></p>
+      <p className="circle" style={{ backgroundColor:`${red ?'red':'silver'}` }}></p>
     </div>
   );
 }
